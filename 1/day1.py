07c230f49ecc410d77a66dab3dd73abaeb11fd2d
Kate Smith <kate.smith@digital.justice.gov.uk>
2024-12-06 12:47:00 +0000

    passes day 1 pt 1

--- 1/day1.py
+++ 1/day1.py
@@ -1,6 +1,24 @@
 class Day1:
-    def __init__(self, total):
-        self.answer = total
+    def __init__(self, file_to_open):
+        self.leftNumbers = []
+        self.rightNumbers = []
+        self.answer = 0
+        f = open(file_to_open, 'r')
+        self.content = f.read().split('\n')
+        f.close()
 
     def calculate(self):
+        for line in self.content:
+            splitLine = line.split(' ')
+            self.leftNumbers.append(int(splitLine[0]))
+            self.rightNumbers.append(int(splitLine.pop()))
+
+        self.leftNumbers.sort()
+        self.rightNumbers.sort()
+
+        count = 0
+        for x in self.leftNumbers:
+            diff = abs(x - self.rightNumbers[count])
+            self.answer += diff
+            count += 1
         return self.answer
--- 1/test_day1.py
+++ 1/test_day1.py
@@ -3,9 +3,13 @@ import day1
 
 class TestDay1(unittest.TestCase):
     def test_part_one(self):
-        kate = day1.Day1(56)
-        assert kate.calculate() == 56, "Should be something"
-        self.assertEqual(56, kate.calculate())
+        kate = day1.Day1("test_input.txt")
+        assert kate.calculate() == 11, "Should be 11"
 
+    def test_real_answer(self):
+        kate = day1.Day1('real_input.txt')
+        realAnswer = kate.calculate()
+        assert realAnswer == 11, "Expected 11 but got " +  str(realAnswer)
+# 494 too low
 if __name__ == '__main__':
     unittest.main()
