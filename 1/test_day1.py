543ac6ce651d78f611526c73a0848a0e3c8c6732
Kate Smith <kate.smith@digital.justice.gov.uk>
2024-12-06 14:05:54 +0000

    finished day 1 pt 2

--- 1/test_day1.py
+++ 1/test_day1.py
@@ -1,15 +1,26 @@
 import unittest
-import day1
+import day1pt1
+import day1pt2
 
 class TestDay1(unittest.TestCase):
     def test_part_one(self):
-        kate = day1.Day1("test_input.txt")
+        kate = day1pt1.Day1("test_input.txt")
         assert kate.calculate() == 11, "Should be 11"
 
     def test_real_answer(self):
-        kate = day1.Day1('real_input.txt')
-        realAnswer = kate.calculate()
-        assert realAnswer == 11, "Expected 11 but got " +  str(realAnswer)
-# 494 too low
+        kate = day1pt1.Day1('real_input.txt')
+        real_answer = kate.calculate()
+        assert real_answer == 2430334, "Expected 2430334"
+
+    def test_part_two(self):
+        kate = day1pt2.Day1Pt2("test_input.txt")
+        assert kate.calculate() == 31, "Should be 31"
+
+    def test_real_answer_part_two(self):
+        kate = day1pt2.Day1Pt2('real_input.txt')
+        real_answer = kate.calculate()
+        assert real_answer == 2430334, "Expected 2430334"
+
+
 if __name__ == '__main__':
     unittest.main()
