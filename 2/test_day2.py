da6c90d150e31965cfb12f181f43e6612dfb95d2
Kate Smith <kate.smith@digital.justice.gov.uk>
2024-12-12 14:21:18 +0000

    day 2 pt 1 passing

--- 2/test_day2.py
+++ 2/test_day2.py
@@ -3,14 +3,14 @@ import day2pt1
 import day2pt2
 
 class TestDay1(unittest.TestCase):
-    # def test_part_one(self):
-    #     kate = day2pt1.Day1("test_input.txt")
-    #     assert kate.calculate() == 2, "Should be 2"
+    def test_part_one(self):
+        kate = day2pt1.Day1("test_input.txt")
+        assert kate.calculate() == 2, "Should be 2"
 
     def test_real_answer(self):
         kate = day2pt1.Day1('real_input.txt')
         real_answer = kate.calculate()
-        assert real_answer == 1, "Expected 1"
+        assert real_answer == 598, "Expected 598"
 
     # def test_part_two(self):
     #     kate = day2pt2.Day1Pt2("test_input.txt")
